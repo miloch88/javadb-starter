3011faccf5abf438b17b9d4f3950f5745783f5fa
Jaroslaw.Skarzynski <jaroslaw.skarzynski@wolterskluwer.com>
2018-06-13 23:08:33 +0200

    library-manager added

---
+++ library-manager/src/main/java/pl/sda/library/manager/DatabaseManager.java
@@ -0,0 +1,17 @@
+package pl.sda.library.manager;
+
+public class DatabaseManager {
+
+
+    public static void createDb() {
+
+    }
+
+    public static void initializeDb() {
+
+    }
+
+    public static void dropDb() {
+
+    }
+}
---
+++ library-manager/src/main/resources/library_db_structure.sql
@@ -0,0 +1,54 @@
+-- -----------------------------------------------------
+-- Table library.readers
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS library.readers (
+  id INT NOT NULL AUTO_INCREMENT,
+  login VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  haslo VARCHAR(45) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  name VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+PRIMARY KEY (id))
+  ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table library.categories
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS library.categories (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+PRIMARY KEY (id))
+  ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table library.books
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS library.books (
+  id INT NOT NULL AUTO_INCREMENT,
+  category_id INT NOT NULL,
+  title VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  author VARCHAR(70) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (category_id)
+REFERENCES library.categories(id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table library.orders
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS library.orders (
+  id INT NOT NULL AUTO_INCREMENT,
+  reader_id INT NOT NULL,
+  book_id INT NOT NULL,
+  order_date DATETIME NOT NULL,
+  return_date DATETIME NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (reader_id)
+REFERENCES library.readers(id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION,
+FOREIGN KEY (book_id)
+REFERENCES library.books(id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
