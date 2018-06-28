7797888d69330d50a62059679f752cd4cd714677
Jaroslaw_Skarzynski <jskarzynski@wolterskluwer.pl>
2018-06-28 13:46:09 +0200

    task-8 + improvements

--- hibernate-starter/src/main/java/pl/sda/hibernate/starter/SimpleHibernateConfiguration.java
+++ hibernate-starter/src/main/java/pl/sda/hibernate/starter/SimpleHibernateConfiguration.java
@@ -13,21 +13,44 @@ public class SimpleHibernateConfiguration {
     private static Logger logger = LoggerFactory.getLogger(SimpleHibernateConfiguration.class);
 
     public static void main(String[] args) {
+        /**
+         * Krok 1: prosta konfiguracja Hibernate: tworzymy obiekt klasy Configuration i
+         * podajemy mu plik z konfiguracją: "hibernate.cfg.xml" - plik znajduje się w katalogu resources
+        */
         Configuration configuration = new Configuration();
         configuration.configure("hibernate.cfg.xml");
 
-        SessionFactory factory = configuration.buildSessionFactory();
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
+        /**
+         * Krok 2: tworzymy dwa obiekty: SessionFactory i Session z konfiguracji, którą wcześniej przygotowaliśmy
+         */
+        try(SessionFactory sessionFactory = configuration.buildSessionFactory();
+            Session session = sessionFactory.openSession()) {
 
-        Course course = new Course("JavaGda11", "Sopot", Utils.parse("2018-01-01"), Utils.parse("2018-09-01"));
-        logger.info("Before: {}", course);
-        Integer id = (Integer) session.save(course);
-        logger.info("Id: {}", id);
-        logger.info("After: {}", course);
+            /**
+             * Krok 3: zaczynamy nową transakcję, każda operacja na bazie danych musi być "otoczona" transakcją
+             */
+            Transaction transaction = session.beginTransaction();
 
-        transaction.commit();
-        session.close();
-        factory.close();
+            Course course = new Course("JavaGda11", "Sopot", Utils.parse("2018-01-01"), Utils.parse("2018-09-01"));
+            logger.info("Before: {}", course);
+            Integer id = (Integer) session.save(course);
+            logger.info("Id: {}", id);
+            logger.info("After: {}", course);
+
+            course = new Course("JavaGda15", "Gdansk", Utils.parse("2018-05-11"), Utils.parse("2018-12-11"));
+            logger.info("Before: {}", course);
+            id = (Integer) session.save(course);
+            logger.info("Id: {}", id);
+            logger.info("After: {}", course);
+
+            /**
+             * Krok 4: kończymy transakcję - wszystkie dane powinny być zapisane w bazie
+             */
+            transaction.commit();
+
+            /**
+             * Krok 5: niejawnie zamykamy: sessionFactory i session
+             */
+        }
     }
 }
