2ee80d44d5d29d4d981576b7582cdc153312356a
Jaroslaw.Skarzynski <jaroslaw.skarzynski@wolterskluwer.com>
2018-06-30 22:42:56 +0200

    task-11

--- jpa-starter/src/main/java/pl/sda/jpa/starter/CourseEntityDao.java
+++ jpa-starter/src/main/java/pl/sda/jpa/starter/CourseEntityDao.java
@@ -2,33 +2,141 @@ package pl.sda.jpa.starter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.sda.commons.Utils;
 import pl.sda.jpa.starter.basic_entities.CourseEntity;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class CourseEntityDao {
     private static Logger logger = LoggerFactory.getLogger(CourseEntityDao.class);
+    private EntityManagerFactory entityManagerFactory;
 
-    public CourseEntity save(CourseEntity courseEntity) {
-        return null;
+    public CourseEntityDao() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("pl.sda.jpa.starter");
     }
 
-    public CourseEntity update(CourseEntity courseEntity) {
-        return null;
-    }
-
-    public void delete(CourseEntity courseEntity) {
-    }
-
-    public CourseEntity findById(int id) {
-        return null;
-    }
-
-    public List<CourseEntity> list() {
-        return null;
+    public void close() {
+        entityManagerFactory.close();
     }
 
     public static void main(String[] args) {
+        CourseEntityDao dao = new CourseEntityDao();
+        try {
+            dao.save(new CourseEntity("JavaGda1", "Rumia", Utils.parse("2018-01-01"), Utils.parse("2018-09-01")));
 
+            /*CourseEntity courseEntity = dao.findById(2);
+            courseEntity.setName("Kurs spadochronowy");
+            courseEntity.setPlace("Warszawa");
+            dao.update(courseEntity);*/
+
+            /*List<CourseEntity> courseEntities = dao.list();
+            if(!courseEntities.isEmpty()) {
+                dao.delete(courseEntities.get(0));
+            }*/
+
+        } catch (Exception e) {
+            logger.error("Error", e);
+        } finally {
+            dao.close();
+        }
+    }
+
+    public CourseEntity save(CourseEntity courseEntity) {
+        EntityManager entityManager = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            entityManager.persist(courseEntity);
+            logger.info("New course added, courseEntity:{}", courseEntity);
+
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+
+        return courseEntity;
+    }
+
+    public CourseEntity update(CourseEntity courseEntity) {
+        EntityManager entityManager = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            entityManager.merge(courseEntity);
+            logger.info("Course updated, courseEntity:{}", courseEntity);
+
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+
+        return courseEntity;
+    }
+
+    public void delete(CourseEntity courseEntity) {
+        EntityManager entityManager = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            courseEntity = entityManager.merge(courseEntity);
+            entityManager.remove(courseEntity);
+            logger.info("Course removed, courseEntity:{}", courseEntity);
+
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+    }
+
+    public CourseEntity findById(int id) {
+        EntityManager entityManager = null;
+        CourseEntity courseEntity;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            courseEntity = entityManager.find(CourseEntity.class, id);
+
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+
+        return courseEntity;
+    }
+
+    public List<CourseEntity> list() {
+        EntityManager entityManager = null;
+        List<CourseEntity> courseEntities;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            TypedQuery<CourseEntity> query = entityManager.createQuery("from CourseEntity", CourseEntity.class);
+            courseEntities = query.getResultList();
+
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+
+        return courseEntities;
     }
 }
--- jpa-starter/src/main/java/pl/sda/jpa/starter/JpaTest.java
+++ jpa-starter/src/main/java/pl/sda/jpa/starter/JpaTest.java
@@ -2,11 +2,140 @@ package pl.sda.jpa.starter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.sda.commons.Utils;
+import pl.sda.jpa.starter.basic_entities.CourseEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.Date;
+import java.util.List;
 
 public class JpaTest {
     private static Logger logger = LoggerFactory.getLogger(JpaTest.class);
+    private EntityManagerFactory entityManagerFactory;
+
+    public JpaTest() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("pl.sda.jpa.starter");
+    }
+
+    public void close() {
+        entityManagerFactory.close();
+    }
 
     public static void main(String[] args) {
+        JpaTest test = new JpaTest();
+        try {
+            //test.addFewEntities();
+            //test.deleteEntity(3);
+            //test.removeLastFromList();
+            test.updateEntity(2, "JAVA_GDA_5_!", Utils.parse("2018-06-01"));
 
+        } catch (Exception e) {
+            logger.error("Error", e);
+        } finally {
+            test.close();
+        }
+    }
+
+    private void updateEntity(int id, String newName, Date newEndDate) {
+        EntityManager entityManager = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            CourseEntity courseEntity = entityManager.find(CourseEntity.class, id);
+            if(courseEntity != null) {
+                courseEntity.setName(newName);
+                courseEntity.setEndDate(newEndDate);
+            } else {
+                logger.warn("CourseEntity of id:{} - not found !", id);
+            }
+
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+    }
+
+    private void removeLastFromList() {
+        EntityManager entityManager = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            TypedQuery<CourseEntity> query = entityManager.createQuery("from CourseEntity", CourseEntity.class);
+            List<CourseEntity> resultList = query.getResultList();
+
+            if(!resultList.isEmpty()) {
+                CourseEntity lastEntity = resultList.get(resultList.size() - 1);
+                entityManager.remove(lastEntity);
+                logger.info("Last entity removed, entity:{}", lastEntity);
+            } else {
+                logger.warn("Can't find any of CourseEntity in database!");
+            }
+
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+    }
+
+    private void deleteEntity(int id) {
+        EntityManager entityManager = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            CourseEntity courseEntity = entityManager.find(CourseEntity.class, id);
+            if (courseEntity != null) {
+                entityManager.remove(courseEntity);
+                logger.info("Entity removed, entity:{}", courseEntity);
+            } else {
+                logger.warn("CourseEntity of id:{} - not found !", id);
+            }
+
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+    }
+
+    private void addFewEntities() {
+        CourseEntity courseEntity1 = new CourseEntity("JavaGda1", "Rumia", Utils.parse("2018-01-01"), Utils.parse("2018-09-01"));
+        CourseEntity courseEntity2 = new CourseEntity("JavaGda5", "Sopot", Utils.parse("2018-04-11"), Utils.parse("2018-12-20"));
+        CourseEntity courseEntity3 = new CourseEntity("JavaGda11", "Gdańsk", Utils.parse("2018-03-01"), Utils.parse("2018-08-31"));
+        CourseEntity courseEntity4 = new CourseEntity("JavaGda16", "Sopot", Utils.parse("2018-11-01"), Utils.parse("2019-04-30"));
+
+        EntityManager entityManager = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            entityManager.persist(courseEntity1);
+            logger.info("New course added, id:{}", courseEntity1.getId());
+
+            entityManager.persist(courseEntity2);
+            logger.info("New course added, id:{}", courseEntity2.getId());
+
+            entityManager.persist(courseEntity3);
+            logger.info("New course added, id:{}", courseEntity3.getId());
+
+            entityManager.persist(courseEntity4);
+            logger.info("New course added, id:{}", courseEntity4.getId());
+
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 }
