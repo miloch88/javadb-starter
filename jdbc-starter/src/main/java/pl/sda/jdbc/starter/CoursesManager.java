0e14b5045ad48a93ccca1161c336f71d95802506
Jaroslaw.Skarzynski <jaroslaw.skarzynski@wolterskluwer.com>
2018-08-23 22:25:35 +0200

    CoursesManager class added

---
+++ jdbc-starter/src/main/java/pl/sda/jdbc/starter/CoursesManager.java
@@ -0,0 +1,20 @@
+package pl.sda.jdbc.starter;
+
+public class CoursesManager {
+
+    public void createCoursesTable(){
+
+    }
+
+    public void createStudentsTable(){
+
+    }
+
+    public void createAttendanceListTable(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
