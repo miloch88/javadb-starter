d1fdfc92c74134b6668e7b5fdaebf317c3b997ba
Jaroslaw.Skarzynski <jaroslaw.skarzynski@wolterskluwer.com>
2018-08-27 23:01:41 +0200

    task-3 refactored

--- jdbc-starter/src/main/java/pl/sda/jdbc/starter/extra/DatabaseDiscovery.java
+++ jdbc-starter/src/main/java/pl/sda/jdbc/starter/extra/DatabaseDiscovery.java
@@ -13,7 +13,7 @@ public class DatabaseDiscovery {
     private static Logger logger = LoggerFactory.getLogger(DatabaseDiscovery.class);
 
     public static void main(String[] args) throws SQLException {
-        try(Connection connection = new ConnectionFactory().getConnection()) {
+        try (Connection connection = new ConnectionFactory("/remote-database.properties").getConnection()) {
             DatabaseMetaData metaData = connection.getMetaData();
 
             logger.info("Database name: {}", connection.getCatalog());
@@ -22,11 +22,14 @@ public class DatabaseDiscovery {
             logger.info("User name: {}\n", metaData.getUserName());
 
             ResultSet resultSet = metaData.getColumns("m1017_sda", null, null, null);
-            while(resultSet.next()) {
+            while (resultSet.next()) {
                 String tableName = resultSet.getString("TABLE_NAME");
                 String columnName = resultSet.getString("COLUMN_NAME");
-                String type = resultSet.getString("TYPE_NAME");
-                logger.info("table: {}, columnName: {}, type: {}", tableName, columnName, type);
+                String columnType = resultSet.getString("TYPE_NAME");
+                String columnSize = resultSet.getString("COLUMN_SIZE");
+                String columnIsNullable = resultSet.getString("IS_NULLABLE");
+                logger.info("table: {}, columnName: {}, columnType: {}, columnSize: {}, columnIsNullable: {}",
+                        tableName, columnName, columnType, columnSize, columnIsNullable);
             }
         }
     }
