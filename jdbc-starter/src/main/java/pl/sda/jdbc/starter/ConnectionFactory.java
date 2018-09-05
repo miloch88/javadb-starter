62f196ae7917214f2f06bfd3fd7390c1718d2a8c
Miloch <michal.miloch@gmail.com>
2018-09-05 10:32:05 +0200

    04 September 2018

--- jdbc-starter/src/main/java/pl/sda/jdbc/starter/ConnectionFactory.java
+++ jdbc-starter/src/main/java/pl/sda/jdbc/starter/ConnectionFactory.java
@@ -1,15 +1,48 @@
 package pl.sda.jdbc.starter;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionFactory {
     private static Logger logger = LoggerFactory.getLogger(ConnectionFactory.class);
+
+    //Tutaj może być singleton, ale my tworzymy coś innego
+
+    private MysqlDataSource dataSource;
+
+    public ConnectionFactory(String filename) {
+
+        try {
+
+            Properties properties = getDataBaseProperties(filename);
+
+            dataSource = new MysqlDataSource();
+            dataSource.setServerName(properties.getProperty("pl.sda.jdbc.db.server"));
+            dataSource.setDatabaseName(properties.getProperty("pl.sda.jdbc.db.name"));
+            dataSource.setUser(properties.getProperty("pl.sda.jdbc.db.user"));
+            dataSource.setPassword(properties.getProperty("pl.sda.jdbc.db.password"));
+            String portString = properties.getProperty("pl.sda.jdbc.db.port");
+            dataSource.setPort(Integer.parseInt(portString));
+            dataSource.setServerTimezone("Europe/Warsaw");
+            dataSource.setUseSSL(false);
+            dataSource.setCharacterEncoding("UTF-8");
+        } catch (SQLException e) {
+            logger.error("Error during creating MysqlDataSource", e);
+            return;
+        }
+    }
+
+    public ConnectionFactory(){
+        //crtl + spacja szuka
+        this("/database.properties");
+    }
 
     private Properties getDataBaseProperties(String filename) {
         Properties properties = new Properties();
@@ -18,7 +51,7 @@ public class ConnectionFactory {
              * Pobieramy zawartość pliku za pomocą classloadera, plik musi znajdować się w katalogu ustawionym w CLASSPATH
              */
             InputStream propertiesStream = ConnectionFactory.class.getResourceAsStream(filename);
-            if(propertiesStream == null) {
+            if (propertiesStream == null) {
                 throw new IllegalArgumentException("Can't find file: " + filename);
             }
             /**
@@ -33,11 +66,16 @@ public class ConnectionFactory {
         return properties;
     }
 
-    public Connection getConnection() {
-        return null;
+    //Taki który trzeba obsłyżć check "Niech się inni martwią"
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        ConnectionFactory connectionFactory =new ConnectionFactory("/remote-database.properties");
+        Connection connection = connectionFactory.getConnection();
+        logger.info(connection.getCatalog());
 
     }
 }
--- jdbc-starter/src/main/java/pl/sda/jdbc/starter/ConnectionViaDataSource.java
+++ jdbc-starter/src/main/java/pl/sda/jdbc/starter/ConnectionViaDataSource.java
@@ -10,10 +10,10 @@ import java.sql.SQLException;
 public class ConnectionViaDataSource {
     private static Logger logger = LoggerFactory.getLogger(ConnectionViaDataSource.class);
 
-    private static final String DB_SERVER_NAME = "";
-    private static final String DB_NAME = "";
-    private static final String DB_USER = "";
-    private static final String DB_PASSWORD = "";
+    private static final String DB_SERVER_NAME = "localhost";
+    private static final String DB_NAME = "cwiczenia";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "miloch";
     private static final int DB_PORT = 3306;
 
     public static void main(String[] args) {
@@ -41,9 +41,12 @@ public class ConnectionViaDataSource {
         /**
          * Krok 2: Otwieramy połączenie do bazy danych
          */
-        Connection connection = null;
-        try {
-            connection = dataSource.getConnection();
+//        Connection connection = null;
+//        try {
+//            connection = dataSource.getConnection();
+
+        try (Connection connection = dataSource.getConnection()) {
+
             logger.info("Connected database successfully...");
 
             /**
@@ -56,17 +59,18 @@ public class ConnectionViaDataSource {
              * Krok 4: Obsługa wyjątków które mogą pojawić się w trakcie pracy z bazą danych
              */
             logger.error("Error during using connection", e);
-        } finally {
-            /**
-             * Krok 5: Zawsze zamykamy połączenie po skończonej pracy!
-             */
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                logger.error("Error during closing connection", e);
-            }
         }
+//        finally {
+//            /**
+//             * Krok 5: Zawsze zamykamy połączenie po skończonej pracy!
+//             */
+//            try {
+//                if (connection != null) {
+//                    connection.close();
+//                }
+//            } catch (SQLException e) {
+//                logger.error("Error during closing connection", e);
+//            }
+//        }
     }
 }
